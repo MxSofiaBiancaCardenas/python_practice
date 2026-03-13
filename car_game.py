27cb1dd9e5c3bd4ee366d04e8ed66292aa5d8f00
SofiaCardenasPlanate <sofia.cardenas@planate.com>
2026-03-13 16:37:42 +0800

    update

--- car_game.py
+++ car_game.py
@@ -1,13 +1,26 @@
 command = ""
-while command != 'quit':
+started = False
+while True:
     command = input("> ").lower()
     if command == "start":
-        print("Car started...")
+        if started:
+            print("Car has already started")
+        else:
+            started = True
+            print("Car started...")
     elif command == "stop":
-        print("Car stopped.")
+        if not started:
+            print("Car has alread stopped")
+        else:
+            started = False
+            print("Car stopped.")
     elif command == "help":
         print("""
         start - to start the car
         stop - to stop the car
         quit - to quit
         """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry, I don't understand that!")
---
+++ list.py
@@ -0,0 +1,18 @@
+# # find the largest number
+# numbers = [3, 6, 2, 10, 8, 4]
+# max = numbers[0]
+# for number in numbers:
+#     if number > max:
+#         max = number
+# print(max)
+
+
+# 2d list
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+for row in matrix:
+    for item in row:
+        print(item)
